15353957c1bca15c4f5f2c40bb8566cd3bb173be
Deepak Sai Pendyala <76196490+deepaksaipendyala@users.noreply.github.com>
2021-12-21 09:47:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+from flask import Flask,render_template,request
+from difflib import SequenceMatcher, Differ
+d = Differ()
+app = Flask('app')
+
+def comparecode(code1,code2):
+    text1 = code1
+    text2 = code2
+    lis=[]    
+    print(type(text1))
+    m = SequenceMatcher(None, text1, text2)
+    a="The similarity between two python files is "+str(round(m.ratio() * 100, 2))+"%"
+    # # print(a)
+    lis.append(a)    
+    b = list(
+        d.compare(text1.splitlines(keepends=True),
+                  text2.splitlines(keepends=True)))
+    print(lis)
+    b=""''.join(b)
+    # # print(b)
+    lis.append(b)
+    return lis
+
+
+@app.route('/')
+def hello_world():
+  return render_template('index.html')
+@app.route('/submit_form',methods=['POST','GET'])
+def Sumbit_form():
+    if request.method=='POST':
+        # try:
+            data=request.form.to_dict()
+            print(data)
+            code1=data["code1"]
+            code2=data["code2"]
+            lis= comparecode(code1,code2)
+            return render_template('index.html',r=lis[0],c=lis[1])
+    #     except:
+    #         return 'didnt save to database'
+    else:
+        return 'woops,Something went wrong'
+        
+app.run(host='0.0.0.0', port=8080)
